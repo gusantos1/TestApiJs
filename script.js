2cf6866cdcf72aca6215128de3087180b75e7b16
Guilherme Silva dos Santos <prgsil@intragov.rj.gov.br>
2021-02-25 14:25:22 -0300

    default

--- script.js
+++ script.js
@@ -2,28 +2,22 @@ function initMap() {
   /**
  * [Criação do map no site ...]
  */
-  
-  var options = 
+  const options = 
   {
     center: {lat: -22.9035107, lng: -43.18508515},
     zoom: 15
   };
 
   map = new google.maps.Map(document.getElementById("map"), options)
-
-};
-
-function addMarker(property){
-  /**
- * [Marcador de localização correspondente a lat e lng]
- * @param  {[object]} foo [{location:{lat: float da latitude, lng: float da longitude}}]
- */
-
+  
+  const iconimg = {
+    url:"https://github.com/gusantos1/TestApiJs/blob/main/views/point.png?raw=true",
+    scaledSize: new google.maps.Size(50, 50),
+  };
   const marker = new google.maps.Marker({
-    position: property.location,
-    map:map,
-    icon: property.imageIcon,
-  })
+    position: {lat: -22.9035107, lng: -43.18508515},
+    map,
+    icon: iconimg,
+  });
 };
 
-addMarker({location:{lat:-22.9035107, lng: -43.18508515}})
